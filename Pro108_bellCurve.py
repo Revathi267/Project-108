4d7705a5f0b45b370a95a4176eb7dc35229de2ea
Revathi267 <71169435+Revathi267@users.noreply.github.com>
2021-06-04 18:31:52 +0530

    Add files via upload

---
+++ Pro108_bellCurve.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import plotly.figure_factory as ff
+import csv
+
+df = pd.read_csv("StudentLevel_data.csv")
+
+fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Average Rating"], show_hist=False,)
+fig.show()
